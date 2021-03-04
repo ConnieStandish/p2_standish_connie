50118e829b54f5260836c8d6906ba8c6b948b757
Connie Standish <conniestand@gmail.com>
2021-03-03 20:33:52 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Slider from https://bxslider.com/
+
+$(document).ready(function(){
+     $('.slider').bxSlider({
+          slideWidth: 550,
+          pager: true,
+          touchEnabled: false,
+          maxSlides: 3,
+          shrinkItems: true,
+     });
+   });
